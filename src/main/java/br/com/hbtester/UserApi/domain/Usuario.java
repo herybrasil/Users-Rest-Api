fff66babb83fd5743a0d2c67b4ccb103c900f61c
jony8 <rivaldo18@gmail.com>
2022-11-23 12:12:39 -0300

    Criação da Classe Usuario

--- src/main/java/br/com/hbtester/UserApi/domain/Usuario.java
+++ src/main/java/br/com/hbtester/UserApi/domain/Usuario.java
@@ -1,5 +1,28 @@
 package br.com.hbtester.UserApi.domain;
 
-public class Usuario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="TB_USUARIOS")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;	
+	@Column(unique = true)
+	private String email;
+	private String senha;
 }
